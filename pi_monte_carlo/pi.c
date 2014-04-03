34196bbfcc88a4941a62c340d347cd0079689f3a
Tom Diggelmann <tdiggelm@gmail.com>
2014-04-04 01:19:28 +0200

    added monte carlo pi calc with openmp

---
+++ pi_monte_carlo/pi.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <omp.h>
+
+// thread safe rand
+static inline double my_rand(unsigned int *seed)
+{
+    return rand_r(seed) / (double)RAND_MAX;
+}
+
+double approx_pi(size_t n)
+{
+    double x;
+    double y;
+    int in = 0;
+    #pragma omp parallel
+    {
+#if _OPENMP
+        unsigned int myseed = omp_get_thread_num() + time(NULL);
+#else
+        unsigned int myseed = time(NULL);
+#endif
+        #pragma omp for private(x, y) reduction(+:in)
+        for (int i = 0; i < n; i++) {
+            x = my_rand(&myseed);
+            y = my_rand(&myseed);
+            if (((x*x)+(y*y)) <= 1.0)
+                in++;
+        }
+    }
+    return 4*(double)in/n;
+}
+
+int main(int argc, const char* argv[])
+{
+    size_t n = (argc == 1) ? 10000 : atol(argv[1]);
+    printf("calculating (%ld iterations)...\n", n);
+    printf("pi = %lf\n", approx_pi(n));
+    return 0;
+}
